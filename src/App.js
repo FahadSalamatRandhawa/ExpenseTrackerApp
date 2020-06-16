5a82eb8118c9a79346392480934d228a834b4279
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2020-06-16 14:49:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
 import { GlobalProvider } from './context/GlobalState';
-import { Room } from './components/background';
 
 function App() {
   return (
